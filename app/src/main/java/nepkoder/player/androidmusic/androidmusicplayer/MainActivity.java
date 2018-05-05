d5bebeaf820729277d3cc9fcdcb38da670eabe7e
Sujan Shrestha <nepkoder@gmail.com>
2018-05-05 10:16:10 +0545

    real project start

--- app/src/main/java/nepkoder/player/androidmusic/androidmusicplayer/MainActivity.java
+++ app/src/main/java/nepkoder/player/androidmusic/androidmusicplayer/MainActivity.java
@@ -5,34 +5,73 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
     private Button playButton;
+    private SeekBar mSeekbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mSeekbar = (SeekBar) findViewById(R.id.mSeekbar);
+        mSeekbar.setMax(mediaPlayer.getDuration());
+
+        mSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mediaPlayer.seekTo(progress);
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
         mediaPlayer = new MediaPlayer();
-        mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.FlirtyMaya);
+        mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.FlirtyMaya); // choosing raw file for media
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                int duration = mp.getDuration();
+                String mDuration = String.valueOf(duration/1000);
+                Toast.makeText(getApplicationContext(),"Duration " +mDuration,Toast.LENGTH_LONG).show();
+            }
+        });
+
 
         playButton = (Button) findViewById(R.id.playButton);
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // check before pausing music
                 if (mediaPlayer.isPlaying()) {
                     // stop and give option to start again
                     pauseMusic();
                 } else {
+                    // start the music
                     startMusic();
                 }
             }
         });
     }
 
+    // pause music and set play button
     public void pauseMusic() {
         if (mediaPlayer != null) {
             mediaPlayer.pause();
@@ -40,10 +79,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // starring music
     public void startMusic() {
         if (mediaPlayer !=null) {
             mediaPlayer.start();
             playButton.setText("Pause");
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mediaPlayer!=null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        super.onDestroy();
     }
 }
